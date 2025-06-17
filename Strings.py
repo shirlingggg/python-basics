590d5e0fc0d29bc8fae0c8344c46db6d41acedc9
shirlingggg <shirleenkabunyi@gmail.com>
2025-06-17 10:24:38 +0300

    additional

---
+++ Strings.py
@@ -0,0 +1,22 @@
+course= "MIT"
+greeting="Hello there"
+firstname="Shirleen"
+
+
+print(course)
+#Accessing an element in a string
+print(course[2])
+print(greeting[0])
+
+#Size of string
+print(len(greeting))
+
+#Modifying a string
+print(greeting.upper())
+print(course.lower())
+
+
+#String concatenation-joining strings
+print(greeting+ firstname)
+print(greeting+ " " +firstname)
+
